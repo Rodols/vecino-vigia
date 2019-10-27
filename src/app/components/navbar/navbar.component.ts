06c48853587b30285383b04fb5da779c8edc4635
rodo <vazquezbenitezrodolfo@gmail.com>
2019-10-27 17:13:57 -0600

    Se cambia la navbar por menu lateral con css y java script sin bootstrap

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { AlertasService } from '../../services/alertas.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { AlertasService } from "../../services/alertas.service";
 import { Router } from "@angular/router";
+import { animateChild } from "@angular/animations";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
   isLogged = false;
-  tipoUser = '';
-  constructor(private authService: AuthService, private alertService: AlertasService, private router: Router) { }
+  tipoUser = "";
+  constructor(
+    private authService: AuthService,
+    private alertService: AlertasService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getCurrentUser();
@@ -23,18 +28,32 @@ export class NavbarComponent implements OnInit {
         this.isLogged = true;
         this.alertService.vecinoUser(auth.uid).subscribe(data => {
           this.tipoUser = data.tipo;
-          this.router.navigate(['home']);
+          this.router.navigate(["home"]);
         });
       } else {
-        this.router.navigate(['login']);
+        this.router.navigate(["login"]);
         this.isLogged = false;
-        this.tipoUser = '';
+        this.tipoUser = "";
       }
     });
   }
 
   outLogin() {
     this.authService.cerrarSession();
+    const burger = document.querySelector(".burger");
+    if (burger.classList.contains("toggle")) {
+      burger.classList.toggle("toggle");
+    }
   }
 
+  menu() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".menu");
+    const navLinks = document.querySelectorAll(".menu li");
+
+    nav.classList.toggle("nav-active");
+
+    //Burger animations
+    burger.classList.toggle("toggle");
+  }
 }
